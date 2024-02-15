ab3c7ff3fec107ae5f0deabe41497aa5f5bdbb8d
markpopovich9 <121514610+markpopovich9@users.noreply.github.com>
2024-02-15 18:21:46 +0200

    5 lesson

--- main.py
+++ main.py
@@ -14,61 +14,115 @@ async def start(message:aiogram.types.Message):
     # m_sqlite.add_column(name_column="hotDog",type_column="INTEGER",name_table=message.from_user.username)
     await message.answer(text="Hello",reply_markup=m_keyboard.create_keyboard([["Адміністратор","Кліент"]]))
     # #await m_image.image("burger.jpg",message,reply_markup=m_keyboard.create_inline_keyboard())
-    
+# @m_data.dp.message()
+# async def moderator(message:aiogram.types.Message):
+#     if message.chat.id==m_data.moderator_id and m_data.list_admin!=[]:
+#         if message.text == "accept":
+#             id = m_data.list_admin[-1][-1]
+#             m_sqlite.set_value(columns=(id,"test"),values=(m_data.users[id]["phone"],message.text),name_table="AdminPhone")
+#             m_sqlite.set_value(columns=(id,"test"),values=(m_data.users[id]["email"],message.text),name_table="AdminEmail")
+#             m_sqlite.set_value(columns=(id,"test"),values=(m_data.users[id]["password"],message.text),name_table="AdminPassword")
+#             m_sqlite.set_value(columns=(id,"test"),values=(m_data.users[id]["username"],message.text),name_table="Administration")
+#             await m_data.bot.send_message(id,"Ви зареєстровані")
+#             del m_data.list_admin[-1]
+#         elif message.text=="no accept":
+#             await m_data.bot.send_message(id,"Ви не зареєстровані")
+            
 @m_data.dp.message()
 async def continue_1(message:aiogram.types.Message):
-    # print(message) 
-    m_sqlite.cursor.execute(f"CREATE TABLE IF NOT EXISTS AdminPassword (INTEGER PRIMARY KEY,id)")
-    m_sqlite.cursor.execute(f"CREATE TABLE IF NOT EXISTS AdminEmail (INTEGER PRIMARY KEY,id)")
-    m_sqlite.cursor.execute(f"CREATE TABLE IF NOT EXISTS AdminPhone (INTEGER PRIMARY KEY,id)")
-    m_sqlite.cursor.execute(f"CREATE TABLE IF NOT EXISTS Administration (INTEGER PRIMARY KEY,id)")
-    m_sqlite.add_column("test","TEXT","AdminPassword")
-    m_sqlite.add_column("test","TEXT","Administration")
-    m_sqlite.add_column("test","TEXT","AdminEmail")
-    m_sqlite.add_column("test","TEXT","AdminPhone")
-    # m_sqlite.add_column(name_column="burger",type_column="INTEGER",name_table=message.from_user.username)
-    # m_sqlite.add_column(name_column="hotDog",type_column="INTEGER",name_table=message.from_user.username)
-    if m_data.reg[0]:
-        if m_data.reg[1]=="password":
-            m_data.reg = [True,"email"]
-            m_data.users[message.chat.username].append(message.text)
-            m_sqlite.set_value(columns=(message.chat.username,"test"),values=(message.text,message.text),name_table="AdminPassword")
-            await message.answer(text="Укажіть свії електрону почту")
-        elif m_data.reg[1]=="email":
-            m_data.reg = [True,"phone"]
-            m_data.users[message.chat.username].append(message.text)
-            m_sqlite.set_value(columns=(message.chat.username,"test"),values=(message.text,message.text),name_table="AdminEmail")
-            await message.answer(text="Укажіть свії номер телефону")
-        elif m_data.reg[1]=="phone":
-            m_data.reg = [False,None]
-            m_data.users[message.chat.username].append(message.text)
-            m_sqlite.set_value(columns=(message.chat.username,"test"),values=(message.text,message.text),name_table="AdminPhone")
+    print(message.chat.id,m_data.moderator_id)
+    if message.chat.id==m_data.moderator_id:
+        id = str(m_data.list_admin[-1][-1])
+        name=str(m_data.list_admin[-1][0])
+        if message.text == "accept":
+            m_sqlite.add_column(name,"TEXT","AdminPassword")
+            m_sqlite.add_column(name,"TEXT","Administration")
+            m_sqlite.add_column(name,"TEXT","AdminEmail")
+            m_sqlite.add_column(name,"TEXT","AdminPhone")
             m_sqlite.data.commit()
-            await message.answer(text="Ви зарегистрировани")
+            m_sqlite.set_value(columns=(name,"test"),values=(m_data.users[id]["phone"],message.text),name_table="AdminPhone")
+            m_sqlite.set_value(columns=(name,"test"),values=(m_data.users[id]["email"],message.text),name_table="AdminEmail")
+            m_sqlite.set_value(columns=(name,"test"),values=(m_data.users[id]["password"],message.text),name_table="AdminPassword")
+            m_sqlite.set_value(columns=(name,"test"),values=(m_data.users[id]["username"],message.text),name_table="Administration")
+            await m_data.bot.send_message(id,"Ви зареєстровані")
+            del m_data.list_admin[-1]
+            m_sqlite.data.commit()
+        elif message.text=="no accept":
+            await m_data.bot.send_message(id,"Ви не зареєстровані")
+            del m_data.list_admin[-1]
     else:
-        if message.text=="Кліент":
-            m_data.user="client"
-            # await m_image.image("burger.jpg",message,reply_markup=m_keyboard.create_inline_keyboard())
-        elif message.text=="Адміністратор":
-            m_data.user="admin"
-            await message.answer(text="Реєстрація або Авторизація",reply_markup=m_keyboard.create_keyboard([["Авторизація","Реєстрація"]]))
-        elif message.text == "Реєстрація"and m_data.user == "admin":
-            try:
-                print(m_sqlite.get_value(message.chat.username,"AdminPhone")[0][0])
-                if not m_sqlite.get_value(message.chat.username,"AdminPhone")[0][0]:
-                    0//0
-                await message.answer(text="Администратор с даним ником уже существуєт")
-            except:
-                m_sqlite.add_column(name_column=message.chat.username,type_column="TEXT",name_table="Administration")
-                m_sqlite.add_column(name_column=message.chat.username,type_column="TEXT",name_table="AdminPassword")
-                m_sqlite.add_column(name_column=message.chat.username,type_column="TEXT",name_table="AdminEmail")
-                m_sqlite.add_column(name_column=message.chat.username,type_column="TEXT",name_table="AdminPhone")
-                m_data.users[message.chat.username]=[message.chat.username]
-                m_data.reg=[True,"password"]
-                m_sqlite.set_value(columns=(message.chat.username,"test"),values=(message.chat.username,message.text),name_table="Administration")
-                await  message.answer(text="Укажіть свії пароль")
-        elif message.text == "Авторизація":
-            pass
+        # print(message) 
+        m_sqlite.cursor.execute(f"CREATE TABLE IF NOT EXISTS AdminPassword (INTEGER PRIMARY KEY,id)")
+        m_sqlite.cursor.execute(f"CREATE TABLE IF NOT EXISTS AdminEmail (INTEGER PRIMARY KEY,id)")
+        m_sqlite.cursor.execute(f"CREATE TABLE IF NOT EXISTS AdminPhone (INTEGER PRIMARY KEY,id)")
+        m_sqlite.cursor.execute(f"CREATE TABLE IF NOT EXISTS Administration (INTEGER PRIMARY KEY,id)")
+        m_sqlite.add_column("test","TEXT","AdminPassword")
+        m_sqlite.add_column("test","TEXT","Administration")
+        m_sqlite.add_column("test","TEXT","AdminEmail")
+        m_sqlite.add_column("test","TEXT","AdminPhone")
+        # m_sqlite.add_column(name_column="burger",type_column="INTEGER",name_table=message.from_user.username)
+        # m_sqlite.add_column(name_column="hotDog",type_column="INTEGER",name_table=message.from_user.username)
+        if m_data.reg[0]:
+            if m_data.reg[1]== "username":
+                m_data.reg = [True,"password"]
+                # print(m_data.users[str(message.chat.id)]["username"])
+                m_data.users[str(message.chat.id)]["username"]=message.text
+                # m_sqlite.set_value(columns=(message.chat.username,"test"),values=(message.text,message.text),name_table="AdminPassword")
+                await message.answer(text="Укажіть свії пароль")
+            elif m_data.reg[1]=="password":
+                m_data.reg = [True,"email"]
+                m_data.users[str(message.chat.id)]["password"]=message.text
+                # m_sqlite.set_value(columns=(message.chat.username,"test"),values=(message.text,message.text),name_table="AdminPassword")
+                await message.answer(text="Укажіть свії електрону почту")
+            elif m_data.reg[1]=="email":
+                m_data.reg = [True,"phone"]
+                m_data.users[str(message.chat.id)]["email"]=message.text
+                # m_sqlite.set_value(columns=(message.chat.username,"test"),values=(message.text,message.text),name_table="AdminEmail")
+                await message.answer(text="Укажіть свії номер телефону")
+            elif m_data.reg[1]=="phone":
+                m_data.reg = [False,None]
+                m_data.users[str(message.chat.id)]["phone"]=message.text
+                # m_sqlite.set_value(columns=(message.chat.username,"test"),values=(message.text,message.text),name_table="AdminPhone")
+                
+                await message.answer(text="Зачекайте підтвердження модератора")
+                text=f"Людина {str(message.chat.first_name)} хоче стати адміном:\n"
+                text+=f"\t username: {m_data.users[str(message.chat.id)]['username']}\n"
+                text+=f"\t password: {m_data.users[str(message.chat.id)]['password']}\n"
+                text+=f"\t email: {m_data.users[str(message.chat.id)]['email']}\n"
+                text+=f"\t phone: {m_data.users[str(message.chat.id)]['phone']}"
+                print(message.chat.first_name,message.chat.id,m_data.list_admin)
+                m_data.list_admin+=[[message.chat.first_name,message.chat.id]]
+                await m_data.bot.send_message(m_data.moderator_id,text,reply_markup=m_keyboard.create_keyboard([["accept","no accept"]]))
+                # await m_data.bot.send_message(m_data.moderator_id,f"\t username: {m_data.users[message.chat.username]['username']}")
+                # await m_data.bot.send_message(m_data.moderator_id,f"\t password: {m_data.users[message.chat.username]['password']}")
+                # await m_data.bot.send_message(m_data.moderator_id,f"\t email: {m_data.users[message.chat.username]['email']}")
+                # await m_data.bot.send_message(m_data.moderator_id,f"\t phone: {m_data.users[message.chat.username]['phone']}")
+                # await message.answer(text="Ви зарегистрировани")
+            
+        else:
+            if message.text=="Кліент":
+                m_data.user="client"
+                # await m_image.image("burger.jpg",message,reply_markup=m_keyboard.create_inline_keyboard())
+            elif message.text=="Адміністратор":
+                m_data.user="admin"
+                await message.answer(text="Реєстрація або Авторизація",reply_markup=m_keyboard.create_keyboard([["Авторизація","Реєстрація"]]))
+            elif message.text == "Реєстрація" and m_data.user == "admin":
+                try:
+                    print(m_sqlite.get_value(message.chat.username,"AdminPhone")[0][0])
+                    if not m_sqlite.get_value(message.chat.username,"AdminPhone")[0][0]:
+                        0//0
+                    await message.answer(text="Администратор с даним ником уже существуєт")
+                except:
+                    m_sqlite.add_column(name_column=message.chat.username,type_column="TEXT",name_table="Administration")
+                    m_sqlite.add_column(name_column=message.chat.username,type_column="TEXT",name_table="AdminPassword")
+                    m_sqlite.add_column(name_column=message.chat.username,type_column="TEXT",name_table="AdminEmail")
+                    m_sqlite.add_column(name_column=message.chat.username,type_column="TEXT",name_table="AdminPhone")
+                    m_data.users[str(message.chat.id)]={"user":message.chat.username}
+                    m_data.reg=[True,"username"]
+                    # m_sqlite.set_value(columns=(message.chat.username,"test"),values=(message.chat.username,message.text),name_table="Administration")
+                    await  message.answer(text="Укажіть свії ім'я пользивателя")
+            elif message.text == "Авторизація":
+                pass
         # await m_image.image("hotDog.jpg",message,reply_markup=m_keyboard.create_inline_keyboard())
 # @m_data.dp.callback_query()
 # async def call_back(callback:aiogram.types.callback_query.CallbackQuery):
--- modules/data.py
+++ modules/data.py
@@ -5,4 +5,7 @@ bot = aiogram.Bot(token="6669027800:AAH0Cj4rJmqArmz5RAsVd0fMfS9uX-XrIFA")
 user=None
 reg = [False,None]
 users = {}
+moderator_id = 2036291862
+list_admin=[]
+# data_agmin=
 # list_admin = 
